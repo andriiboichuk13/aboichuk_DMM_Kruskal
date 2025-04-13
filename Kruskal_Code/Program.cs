13db34802bc0cc17522f3f0ce155913122f31294
andriiboichuk13 <aboichuk06@gmail.com>
2025-04-13 22:30:36 +0300

    change to printing

--- Kruskal_Code/Program.cs
+++ Kruskal_Code/Program.cs
@@ -3,20 +3,21 @@ Console.OutputEncoding = System.Text.Encoding.UTF8;
 
 MapGenerator map = new MapGenerator();
 Kruskal kruskal = new Kruskal();
-var res = map.GenerateMap(20, 0.1);
+var res = map.GenerateMap(50, 0.5);
 map.mapPrint(res);
 var list = map.adjList(res);
 
 //var mintree =kruskal.MinSpanTree(list, 200);
-var mintree = kruskal.MinSpanTree(res, 20);
+var mintree = kruskal.MinSpanTree(res, 50);
 
 
 if (mintree != null)
 {
+    Console.WriteLine("\n");
+    Console.WriteLine("Minimal span tree for this graph is: ");
     foreach (var edge in mintree)
     {
-        Console.WriteLine("\n");
-        Console.WriteLine("Minimal span tree for this graph is: ");
+        
         Console.WriteLine($"{edge.coord1+1}------({edge.val})------{edge.coord2+1}");
     }
 }
